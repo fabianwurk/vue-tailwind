69280331cbc1dce1fb45815fcdaec0f02e694c82
fabianwurk <fabian@wurkhouse.com>
2021-03-19 01:16:34 +0000

    Add hero option

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -44,8 +44,8 @@
 
     <div class="mx-auto bg-white" v-bind:class="selectedViewport">
       <div class="flex items-center justify-center">
-        <div class="bg-blue-300 mb-4 mt-0 text-center" v-bind:class="selectedColumn,selectedPadding,selectedHeight">
-          <span class="bg-blue-200 w-full flex text-1xl font-bold justify-center items-center" ref="boxWidth" id="divw" v-bind:class="selectedHeight">{{width}}px</span>
+        <div class="bg-black mb-4 mt-0 text-center" v-bind:class="selectedColumn,selectedPadding,selectedHeight">
+          <span class="bg-blue-200 w-full flex text-1xl font-bold justify-center items-center bg-cover bg-no-repeat bg-center" ref="boxWidth" id="divw" v-bind:class="selectedHeight"  style="background-image: url('https://picsum.photos/1200/');">{{width}}px</span>
         </div>
       </div>
     </div>
@@ -60,11 +60,13 @@ export default {
       width: 0,
       selectedViewport: 'container',
       voptions: [
+        { text: 'Hero', value: 'w-full' },
         { text: 'Desktop', value: 'container' },
         { text: 'Tablet Landscape / Laptop', value: 'max-w-screen-lg' },
         { text: 'Tablet Portrait', value: 'max-w-screen-md' },
         { text: 'Small - Mobile Landscape', value: 'max-w-screen-sm' },
         { text: 'Mobile Portrait', value: 'max-w-mobile' },
+
       ],
       selectedColumn: 'w-full',
       coptions: [
@@ -74,14 +76,15 @@ export default {
         { text: 'Third', value: 'w-1/3' },
         { text: 'Quarter', value: 'w-1/4' },
       ],
-      selectedPadding: 'px-4',
+      selectedPadding: 'px-0',
       poptions: [
         { text: 'No padding', value: 'px-0' },
-        { text: '16px (Default)', value: 'px-4' },
-        { text: '24px', value: 'px-6' },
-        { text: '32px', value: 'px-8' },
+        { text: '16px (Black)', value: 'px-4 bg-black' },
+        { text: '16px (White)', value: 'px-4 bg-white' },
+        { text: '32px (Black)', value: 'px-8 bg-black' },
+        { text: '32px (White)', value: 'px-8 bg-white' },
       ],
-      selectedHeight: 'h-8',
+      selectedHeight: 'h-128',
       hoptions: [
         { text: '32px', value: 'h-8' },
         { text: '128px', value: 'h-32' },
